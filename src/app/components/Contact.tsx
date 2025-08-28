05f0df3ddac42b345ff7b7858c710bcc03157762
therealteejay25 <therealteejay25@gmail.com>
2025-08-28 19:35:29 +0100

    Refactor Contact component layout for improved responsiveness

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -2,7 +2,7 @@ import React from 'react'
  
  const Contact = () => {
    return (
-     <div className='h-96 bg-white w-screen flex flex-col items-center justify-center'>
+     <div className='h-96 mx-5 md:mx-0 bg-white w-screen flex flex-col items-center justify-center'>
         <h1 className='text-5xl font-bold text-indigo-500 '>Contact Us!</h1>
         <p className='text-lg my-6 text-center  max-w-md w-ful mx-3 text-gray-700'>Got an idea, project, or collab in mind? Let’s build something amazing together—reach out anytime.</p>
         <button className="button my-5"><a href="mailto:therealteejay25@gmail.com">Reach out to us!</a></button>
